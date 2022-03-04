02cfd664454b2ba43704e7d7354bf58f1275e7ac
Quentin Ikeno <ikenoq@gmail.com>
2022-03-03 19:01:33 -1000

    Create soccer_league schema

---
+++ soccer_leage.sql
@@ -0,0 +1,42 @@
+-- Part Three: Soccer League
+
+DROP DATABASE IF EXISTS soccer_league;
+CREATE DATABASE soccer_league;
+\c soccer_league;
+
+CREATE TABLE teams (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    rank INTEGER NOT NULL
+);
+
+CREATE TABLE goals (
+    id SERIAL PRIMARY KEY,
+    player_id INTEGER NOT NULL REFERENCES players ON DELETE SET NULL,
+    match_id INTEGER NOT NULL REFERENCES matches ON DELETE SET NULL
+);
+
+CREATE TABLE players (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    team_id INTEGER NOT NULL REFERENCES teams ON DELETE SET NULL
+);
+
+CREATE TABLE referees (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE matches (
+    id SERIAL PRIMARY KEY,
+    home_team_id INTEGER NOT NULL REFERENCES teams ON DELETE SET NULL,
+    away_team_id INTEGER NOT NULL REFERENCES teams ON DELETE SET NULL,
+    season_id INTEGER NOT NULL REFERENCES season ON DELETE SET NULL,
+    referee_id INTEGER NOT NULL REFERENCES referees ON DELETE SET NULL
+);
+
+CREATE TABLE season (
+    id SERIAL PRIMARY KEY,
+    start_date DATE NOT NULL,
+    end_data DATE NOT NULL
+);
