aa8c702b8665d3c34e3e04aeb78145e83eccbff6
Quentin Ikeno <ikenoq@gmail.com>
2022-03-04 18:27:12 -1000

    Refactor music.sql

--- music.sql
+++ music.sql
@@ -7,27 +7,89 @@ CREATE DATABASE music;
 
 \c music
 
+CREATE TABLE artists
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE albums
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE producers
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE songs
 (
   id SERIAL PRIMARY KEY,
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album_id INTEGER REFERENCES albums
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+CREATE TABLE artists_songs
+(
+  id SERIAL PRIMARY KEY,
+  song_id INTEGER REFERENCES songs,
+  artist_id INTEGER REFERENCES artists
+);
+
+CREATE TABLE producers_songs
+(
+  id SERIAL PRIMARY KEY,
+  song_id INTEGER REFERENCES songs,
+  producer_id INTEGER REFERENCES producers
+);
+
+INSERT INTO artists
+  (name)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('Hanson'),
+  ('Queen'),
+  ('Mariah Carey'),
+  ('Boyz II Men');
+
+INSERT INTO albums
+  (name)
+VALUES
+  ('Middle of Nowhere'),
+  ('A Night at the Opera'),
+  ('Daydream');
+
+INSERT INTO producers
+  (name)
+VALUES
+  ('Dust Brothers'),
+  ('Stephen Lironi'),
+  ('Roy Thomas Baker'),
+  ('Walter Afanasieff');
+
+INSERT INTO songs
+  (title, duration_in_seconds, release_date, album_id)
+VALUES
+  ('MMMBop', 238, '04-15-1997', 1),
+  ('Bohemian Rhapsody', 355, '10-31-1975', 2),
+  ('One Sweet Day', 282, '11-14-1995', 3);
+
+INSERT INTO artists_songs
+  (song_id, artist_id)
+VALUES
+  (1, 1),
+  (2, 2),
+  (3, 3),
+  (3, 4);
+
+INSERT INTO producers_songs
+  (song_id, producer_id)
+VALUES
+  (1, 1),
+  (1, 2),
+  (2, 3),
+  (3, 4);
