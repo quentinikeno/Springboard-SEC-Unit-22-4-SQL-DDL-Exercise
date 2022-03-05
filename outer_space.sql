2b96e0098d4a99a322be43e9cfeb1ff8639057b6
Quentin Ikeno <ikenoq@gmail.com>
2022-03-04 17:23:40 -1000

    Refactor outer space

--- outer_space.sql
+++ outer_space.sql
@@ -7,22 +7,66 @@ CREATE DATABASE outer_space;
 
 \c outer_space
 
+CREATE TABLE stars
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE galaxies
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
 CREATE TABLE planets
 (
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+  orbits_star_id INTEGER REFERENCES stars ON DELETE SET NULL,
+  galaxy_id INTEGER REFERENCES galaxies ON DELETE CASCADE
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+CREATE TABLE moons
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE planets_moons
+(
+  id SERIAL PRIMARY KEY,
+  planet_id INTEGER REFERENCES planets ON DELETE CASCADE,
+  moon_id INTEGER REFERENCES moons ON DELETE SET NULL
+);
+
+INSERT INTO stars
+  (name)
 VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  ('The Sun');
+
+INSERT INTO galaxies
+  (name)
+VALUES
+  ('Milky Way');
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbits_star_id, galaxy_id)
+VALUES
+  ('Earth', 1.00, 1, 1),
+  ('Mars', 1.88, 1, 1);
+
+INSERT INTO moons
+  (name)
+VALUES
+  ('The Moon'),
+  ('Phobos'),
+  ('Deimos');
+
+INSERT INTO planets_moons
+  (planet_id, moon_id)
+VALUES
+  (1, 1),
+  (2, 2),
+  (2, 3);
